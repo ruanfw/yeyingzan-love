9a1760fa997093340a9683a149984545bc83fc26
xingjue <xingjue@qunhemail.com>
2020-06-24 10:57:04 +0800

    init

---
+++ src/main/java/com/rfw/yeyingzan/love/YeyingzanLoveApplication.java
@@ -0,0 +1,13 @@
+package com.rfw.yeyingzan.love;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeyingzanLoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeyingzanLoveApplication.class, args);
+	}
+
+}

0f0636a9f6cb956b27ae0822854620124d6950a0
xingjue <xingjue@qunhemail.com>
2020-07-09 20:14:49 +0800

    增加模板

--- src/main/java/com/rfw/yeyingzan/love/YeyingzanLoveApplication.java
+++ src/main/java/com/rfw/yeyingzan/love/YeyingzanLoveApplication.java
@@ -2,9 +2,16 @@ package com.rfw.yeyingzan.love;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class YeyingzanLoveApplication {
+public class YeyingzanLoveApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+        return application.sources(YeyingzanLoveApplication.class);
+    }
 
     public static void main(final String[] args) {
         SpringApplication.run(YeyingzanLoveApplication.class, args);
---
+++ src/main/java/com/rfw/yeyingzan/love/controller/ThymeleafController.java
@@ -0,0 +1,32 @@
+package com.rfw.yeyingzan.love.controller;
+
+import com.google.common.collect.Lists;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping
+public class ThymeleafController {
+
+    @GetMapping("/index")
+    public ModelAndView index() {
+        final ModelAndView view = new ModelAndView();
+        // 设置跳转的视图 默认映射到 src/main/resources/templates/{viewName}.html
+        view.setViewName("index");
+
+        // 2020-05-20
+        long beginDay = 1589904000000L;
+
+        int days = (int) ((System.currentTimeMillis() - beginDay) / (1000 * 3600 * 24));
+
+        List<String> list = Lists.newArrayList("1234","2345");
+
+        view.addObject("days", days);
+        view.addObject("list", list);
+        return view;
+    }
+}
